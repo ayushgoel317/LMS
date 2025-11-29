5c29be42c28a52e5ed3d29bd59b911d0720f17a1
Ayush goel <82891154+ayushgoel317@users.noreply.github.com>
2025-11-29 12:01:19 +0530

    update authController for backend deploy

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -34,8 +34,8 @@ export const signUp=async (req,res)=>{
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -60,8 +60,8 @@ export const login=async(req,res)=>{
         let token =await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -97,8 +97,8 @@ export const googleSignup = async (req,res) => {
         let token =await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -169,4 +169,5 @@ export const resetPassword = async (req,res) => {
     } catch (error) {
         return res.status(500).json({message:`Reset Password error ${error}`})
     }
+
 }
